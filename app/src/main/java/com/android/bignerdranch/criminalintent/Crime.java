5b309b2d0b8db563239b90b315c49d25a0cd020e
C. Chang <conradc88@yahoo.com>
2015-04-23 11:56:49 +0800

    Next: Implicit Intents

--- app/src/main/java/com/android/bignerdranch/criminalintent/Crime.java
+++ app/src/main/java/com/android/bignerdranch/criminalintent/Crime.java
@@ -15,6 +15,7 @@ public class Crime {
     private static final String JSON_SOLVED = "solved";
     public static final String JSON_DATE = "date";
     public static final String JSON_PHOTO = "photo";
+    public static final String JSON_SUSPECT = "suspect";
 
 
     private UUID mId;
@@ -22,6 +23,7 @@ public class Crime {
     private Date mDate;
     private boolean mSolved;
     private Photo mPhoto;
+    private String mSuspect;
 
 
     public Crime() {
@@ -38,6 +40,8 @@ public class Crime {
         mDate = new Date(json.getLong(JSON_DATE));
         if (json.has(JSON_PHOTO))
             mPhoto = new Photo(json.getJSONObject(JSON_PHOTO));
+        if (json.has(JSON_SUSPECT))
+            mSuspect = json.getString(JSON_SUSPECT);
     }
 
     public UUID getId() {
@@ -86,6 +90,7 @@ public class Crime {
         json.put(JSON_DATE, mDate.getTime());
         if (mPhoto != null)
             json.put(JSON_PHOTO, mPhoto.toJSON());
+            json.put(JSON_SUSPECT, mSuspect);
         return json;
     }
 
@@ -97,4 +102,15 @@ public class Crime {
     public void setPhoto(Photo p) {
         mPhoto = p;
     }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
+    }
+
 }
+
+
--- app/src/main/java/com/android/bignerdranch/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/android/bignerdranch/criminalintent/CrimeFragment.java
@@ -13,6 +13,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -224,4 +225,27 @@ public class CrimeFragment extends Fragment {
         PictureUtils.cleanImageView(mPhotoView);
 
     }
+
+    private String getCrimeReport() {
+        String solvedString;
+        if (mCrime.isSolved()) {
+            solvedString = getString(R.string.crime_report_solved);
+        } else {
+            solvedString = getString(R.string.crime_report_unsolved);
+        }
+
+        String dateFormat = "EEE, MMM dd";
+        String dateString = DateFormat.format(dateFormat, mCrime.getDate()).toString();
+        String suspect = mCrime.getSuspect();
+        if (suspect == null) {
+            suspect = getString(R.string.crime_report_no_suspect);
+        } else {
+            suspect = getString(R.string.crime_report_suspect);
+        }
+
+        String report = getString(R.string.crime_report, mCrime.getTitle(), dateString, solvedString, suspect);
+        return report;
+    }
+
+
 }
